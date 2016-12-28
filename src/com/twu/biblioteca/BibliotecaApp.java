9314cfad160fcf269e6b3e10d42c8cf943d5f1fe
Katrina Lee <katrinamarielee@gmail.com>
2016-12-28 22:01:20 +1100

    Added menu and book check out

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -13,10 +13,10 @@ public class BibliotecaApp {
     }
 
     public ArrayList<Book>  displayBookList(){
-        Book b1 =  new Book("Head First Java");
-        Book b2 = new Book("Test-Driven Development");
-        Book b3 = new Book("Agile Fundamentals");
-        Book b4 = new Book("Ruby Ninjas");
+        Book b1 =  new Book("Head First Java", "Kathy Sierra", 2003);
+        Book b2 = new Book("Test-Driven Development", "Kent Beck", 2003);
+        Book b3 = new Book("Agile Fundamentals", "Someone", 2010);
+        Book b4 = new Book("Ruby Ninjas", "Joel", 1909);
 
         books.add(b1);
         books.add(b2);
@@ -27,26 +27,75 @@ public class BibliotecaApp {
 
     }
 
+    public String checkoutBook(String bookToCheckout){
+        displayBookList();
+        if (books.contains(bookToCheckout)){
+            int index = books.indexOf(bookToCheckout);
+            books.remove(index);
+            System.out.println("Thanks for checking out " + bookToCheckout);
+
+        } else {
+            System.out.println("This book is not currently available.");
+        }
+        return bookToCheckout;
+    }
+
+    public void menu() {
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose from these choices");
+        System.out.println("1 - Display list of books");
+        System.out.println("2 - Check out book");
+        System.out.println("3 - Return book");
+        System.out.println("4 - Quit");
+
+        selection = input.nextInt();
+//        return selection;
+        switch(selection)
+        {
+            case 1:
+                displayBookList();
+                break;
+            case 2:
+                System.out.println("What book would you like to check out?");
+                Scanner bookName = new Scanner(System.in);
+                String bookToCheckOut = bookName.nextLine();
+                checkoutBook(bookToCheckOut);
+                break;
+            case 3:
+//                returnBook();
+                break;
+            case 4:
+                System.out.println("-----------");
+                break;
+            default:
+                System.out.println("Select a valid option!");
+
+        }
+
+    }
+
     public static void main(String[] args) {
         BibliotecaApp user = new BibliotecaApp();
         user.welcomeMessage();
+        user.menu();
         user.displayBookList();
     }
 }
 
 
-//    Welcome Message  - As a customer, I would like to see a welcome message when I start the application, so that I feel welcome and know that Biblioteca is available.
+//    *Welcome Message  - As a customer, I would like to see a welcome message when I start the application, so that I feel welcome and know that Biblioteca is available.
 //print a welcome message introducing biblioteca
-
-
-//        List Books - As a customer, after the welcome message appears I would like to see a list of all library books, so that I can browse for books I might want to check-out. Assume that there is a pre-existing list of books. You don't need to support adding or removing books from the library.
-//        Book Details - As a customer, I'd like the list of all books to include each books Author and Year Published, so that I can be confident that I have found the book I am looking for. The book listing should have columns for this information.
-//        Main Menu  - As a customer, instead of automatically seeing the book list, I would like to see a list of options and be able to choose one. For now, the only option should be 'List Books'. All future options should be added to this menu also.
-//        Invalid Menu Option - As a customer, I would like to be notified when I choose an invalid option with the message “Select a valid option!”, so that I can know when I need to re-enter my choice.
-//        Quit - As a customer, I would like to continue choosing options until I choose to 'Quit'.
-//        Checkout Book - As a librarian, I would like customers to be able to check-out a book. Checked out books should not appear in the list of all library books.
-//        Successful Checkout - As a customer, I would like to know that a book has been checked out by seeing the message “Thank you! Enjoy the book”.
-//        Unsuccessful Checkout - As a customer, I would like to be notified if the book I tried to check-out is not available by seeing the message, “That book is not available.”, so that I know that I need to select a different book or fix my spelling error.
+//        *List Books - As a customer, after the welcome message appears I would like to see a list of all library books, so that I can browse for books I might want to check-out. Assume that there is a pre-existing list of books. You don't need to support adding or removing books from the library.
+//        *Book Details - As a customer, I'd like the list of all books to include each books Author and Year Published, so that I can be confident that I have found the book I am looking for. The book listing should have columns for this information.
+//        *Main Menu  - As a customer, instead of automatically seeing the book list, I would like to see a list of options and be able to choose one. For now, the only option should be 'List Books'. All future options should be added to this menu also.
+//        *Invalid Menu Option - As a customer, I would like to be notified when I choose an invalid option with the message “Select a valid option!”, so that I can know when I need to re-enter my choice.
+//        *Quit - As a customer, I would like to continue choosing options until I choose to 'Quit'.
+//        *Checkout Book - As a librarian, I would like customers to be able to check-out a book. Checked out books should not appear in the list of all library books.
+//        *Successful Checkout - As a customer, I would like to know that a book has been checked out by seeing the message “Thank you! Enjoy the book”.
+//        *Unsuccessful Checkout - As a customer, I would like to be notified if the book I tried to check-out is not available by seeing the message, “That book is not available.”, so that I know that I need to select a different book or fix my spelling error.
 //        Return Book - As a librarian, I would like customers to be able to return a book, so that other customers can check that book out. Returned books should appear in the list of library books.
 //        Successful Return - As a customer, I would like to be notified if the book I am returning belongs to this library by seeing the message, “Thank you for returning the book.”, so that I know I returned the book to the right library.
 //        Unsuccessful Return - As a customer, I would like to be notified if the book I am returning has not been added to this library by seeing the message, “That is not a valid book to return.”, so that I can return it to the correct library or fix my spelling error.
--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -3,8 +3,12 @@ package com.twu.biblioteca;
 public class Book
 {
     private String title;
+    private String author;
+    private int year;
 
-    public Book(String title) {
+    public Book(String title, String author, int year) {
         this.title = title;
+        this.author = author;
+        this.year = year;
     }
 }
